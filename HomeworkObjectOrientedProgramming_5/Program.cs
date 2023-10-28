3933b46f9af041bd25d31429850493326f838833
sasha <sasha21l@mail.ru>
2023-10-28 10:50:37 +0500

    ООП 5

---
+++ HomeworkObjectOrientedProgramming_5/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace HomeworkObjectOrientedProgramming_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class RationalNumbers
+    {
+        public int numerator { get; set; }
+        public int denominator { get; set; }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+
+        public static RationalNumbers operator +(RationalNumbers first, RationalNumbers second)
+        {
+            var result = new RationalNumbers
+            {
+                numerator = first.numerator + second.numerator,
+                denominator = first.denominator + second.denominator
+            };
+            return result;
+      
+        }
+
+        public static RationalNumbers operator -(RationalNumbers first, RationalNumbers second)
+        {
+            var result = new RationalNumbers
+            {
+                numerator = first.numerator - second.numerator,
+                denominator = first.denominator - second.denominator
+            };
+            return result;
+        }
+
+        public static bool operator ==(RationalNumbers first, RationalNumbers second)
+        {
+            if (first is null && second is null) return false;
+            if (first.Equals(second)) return true;
+            else return false;
+        }
+
+        public static bool operator !=(RationalNumbers first, RationalNumbers second)
+        {
+            if (first is null && second is null) return true;
+            if (first.Equals(second)) return false;
+            else return true;
+
+        }
+
+        public static bool operator <(RationalNumbers first, RationalNumbers second)
+        {
+            if (first < second) return true;
+            else return false;
+        }
+
+        public static bool operator >(RationalNumbers first, RationalNumbers second)
+        {
+            if (first < second) return false;
+            else return true;
+
+        }
+
+        public static bool operator <=(RationalNumbers first, RationalNumbers second)
+        {
+            if (first is null && second is null) return false;
+            if (first.Equals(second) || first < second) return true;
+            else return false;
+        }
+
+        public static bool operator >=(RationalNumbers first, RationalNumbers second)
+        {
+            if (first is null && second is null) return true;
+            if (first.Equals(second) || first < second) return false;
+            else return true;
+
+        }
+
+        public static RationalNumbers operator *(RationalNumbers first, RationalNumbers second)
+        {
+            var result = new RationalNumbers
+            {
+                numerator = first.numerator * second.numerator,
+                denominator = first.denominator * second.denominator
+            };
+            return result;
+        }
+
+        public static RationalNumbers operator /(RationalNumbers first, RationalNumbers second)
+        {
+            var result = new RationalNumbers
+            {
+                numerator = first.numerator / second.numerator,
+                denominator = first.denominator / second.denominator
+            };
+            return result;
+        }
+
+        public static RationalNumbers operator %(RationalNumbers first, RationalNumbers second)
+        {
+            var result = new RationalNumbers
+            {
+                numerator = first.numerator % second.numerator,
+                denominator = first.denominator % second.denominator
+            };
+            return result;
+        }
+
+    }
+}
